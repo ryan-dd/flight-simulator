b668d8a985e90f45f3240a8b59f48eade189efb3
Ryan Day <rddays@gmail.com>
2020-02-05 13:33:22 -0700

    Input the first state space equation

--- chap5/compute_models.py
+++ chap5/compute_models.py
@@ -7,6 +7,7 @@ compute_ss_model
 import sys
 sys.path.append('..')
 import numpy as np
+from math import sin, cos, tan
 from scipy.optimize import minimize
 from tools.tools import Euler2Quaternion, Quaternion2Euler
 from tools.transfer_function import TransferFunction
@@ -36,7 +37,7 @@ def compute_tf_model(mav, trim_state, trim_input):
     T_h_Va = TransferFunction(np.array([[theta]]), np.array([[1]]), Ts)
 
 
-    Va_star = trim_state._
+    Va_star = trim_state[3]
     alpha_star = MAV.alpha0
     delta_e_star = trim_input[0].item(0)
     delta_t_star = trim_input[1].item(0) 
@@ -50,7 +51,68 @@ def compute_tf_model(mav, trim_state, trim_input):
     return T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r
 
 def compute_ss_model(mav, trim_state, trim_input):
-     return A_lon, B_lon, A_lat, B_lat
+    ustar = trim_state.item(3)
+    vstar = trim_state.item(4)
+    wstar = trim_state.item(5)
+    e = trim_state[5:9]
+    phistar, thetastar, psistar = Quaternion2Euler(e)
+    pstar = trim_state.item(10)
+    qstar = trim_state.item(11)
+    rstar = trim_state.item(12)
+    delta_e_star = trim_input[0].item(0)
+    delta_t_star = trim_input[1].item(0) 
+    delta_a_star = trim_input[2].item(0) 
+    delta_r_star = trim_input[3].item(0)
+    betastar = 0
+    Vastar = ustar
+    Yv = MAV.rho*MAV.S_wing*MAV.b*vstar/(4*MAV.mass*Vastar)*(MAV.C_Y_p*pstar + MAV.C_Y_r*rstar) + MAV.rho* MAV.S_wing*vstar/(MAV.mass) * (
+        MAV.C_Y_0 + MAV.C_Y_beta*betastar + MAV.C_Y_delta_a*delta_a_star + MAV.C_Y_delta_r*rstar) + MAV.rho*MAV.S_wing*MAV.C_Y_beta/(2*MAV.mass)*np.sqrt(ustar**2 + wstar**2)
+    Yp = wstar + MAV.rho*Vastar*MAV.S_wing*MAV.b/(4*MAV.mass) * MAV.C_Y_p
+    Yr = -ustar + MAV.rho*Vastar*MAV.S_wing*MAV.b/(4*MAV.mass) * MAV.C_Y_r
+    Y_delta_a = MAV.rho*Vastar**2 * MAV.S_wing/(2*MAV.mass) * MAV.C_Y_delta_a
+    Y_delta_r = MAV.rho*Vastar**2 * MAV.S_wing/(2*MAV.mass) * MAV.C_Y_delta_r
+
+    Lv = MAV.rho*MAV.S_wing*MAV.b**2 * vstar/(4*Vastar)*(MAV.C_p_p*pstar + MAV.C_p_r*rstar) + MAV.rho*MAV.S_wing*MAV.b*vstar * (
+        MAV.C_p_0 + MAV.C_p_beta*betastar + MAV.C_p_delta_a*delta_a_star + MAV.C_p_delta_r*rstar) + MAV.rho*MAV.S_wing*MAV.b*MAV.C_p_beta/2*np.sqrt(ustar**2+wstar**2)
+    Lp = MAV.gamma1*qstar + MAV.rho*Vastar*MAV.S_wing*MAV.b**2/4*MAV.C_p_p
+    Lr = -MAV.gamma2*qstar + MAV.rho*Vastar*MAV.S_wing*MAV.b**2/4*MAV.C_p_r
+    L_delta_a = MAV.rho*Vastar**2*MAV.S_wing*MAV.b/2*MAV.C_p_delta_a
+    L_delta_r = MAV.rho*Vastar**2*MAV.S_wing*MAV.b/2*MAV.C_p_delta_r
+
+    Nv = MAV.rho*MAV.S_wing*MAV.b**2 * vstar/(4*Vastar)*(MAV.C_r_p*pstar + MAV.C_r_r*rstar) + MAV.rho*MAV.S_wing*MAV.b*vstar * (
+        MAV.C_r_0 + MAV.C_r_beta*betastar + MAV.C_r_delta_a*delta_a_star + MAV.C_r_delta_r*rstar) + MAV.rho*MAV.S_wing*MAV.b*MAV.C_r_beta/2*np.sqrt(ustar**2+wstar**2)
+    Np = MAV.gamma7*qstar + MAV.rho*Vastar*MAV.S_wing*MAV.b**2/4*MAV.C_r_p
+    Nr = -MAV.gamma1*qstar + MAV.rho*Vastar*MAV.S_wing*MAV.b**2/4*MAV.C_r_r
+    N_delta_a = MAV.rho*Vastar**2*MAV.S_wing*MAV.b/2*MAV.C_r_delta_a
+    N_delta_r = MAV.rho*Vastar**2*MAV.S_wing*MAV.b/2*MAV.C_r_delta_r
+
+    A_lat = np.array([
+        [Yv, Yp, Yr, MAV.gravity*np.cos(thetastar)*np.cos(phistar), 0],
+        [Lv, Lp, Lr, 0, 0],
+        [Nv, Np, Nr, 0, 0],
+        [0, 1, np.cos(phistar)*np.tan(thetastar), qstar*np.cos(phistar)*np.tan(thetastar) - rstar*np.sin(phistar)*np.tan(thetastar), 0],
+        [0, 0, np.cos(phistar)*np.sec(thetastar), pstar*np.cos(phistar)*np.sec(thetastar) - rstar*np.sin(phistar)*np.sec(thetastar), 0]
+        ])
+    B_lat = np.array([
+        [Y_delta_a, Y_delta_r],
+        [L_delta_a, L_delta_r],
+        [N_delta_a, N_delta_r],
+        [0,0],
+        [0,0]])
+    # Longitudinal dynamics
+    sigma_a = (1 + np.exp(-MAV.M*(alpha-MAV.alpha0)) + np.exp(MAV.M*(alpha+MAV.alpha0))) / (1 + np.exp(-MAV.M*(alpha-MAV.alpha0))*(1 + np.exp(MAV.M*(alpha+MAV.alpha0))))
+    C_L_alpha_f = (1 - sigma_a)*(MAV.C_L_0 + MAV.C_L_alpha*alpha) + sigma_a*(2*np.sign(alpha)*np.sin(alpha)**2*alpha*np.cos(alpha))
+    C_D_alpha_f = MAV.C_D_p + (MAV.C_L_0 + MAV.C_L_alpha*alpha)**2/(np.pi*MAV.e*MAV.AR)
+    
+    
+    C_X_alpha = -C_D_alpha_f*cos(alpha) + C_L_alpha_f*sin(alpha)
+    C_X_q_alpha = -MAV.C_D_q*cos(alpha) + MAV.C_L_q*sin(alpha)
+    C_X_delta_e = -MAV.C_D_delta_e*cos(alpha) + MAV.C_L_delta_e*sin(alpha)
+    C_Z_alpha = -C_D_alpha_f*sin(alpha) - C_L_alpha_f*cos(alpha)
+    C_Z_q = -MAV.C_D_q*sin(alpha) - MAV.C_L_q * cos(alpha)
+    C_Z_delta_e = -MAV.C_D_delta_e*sin(alpha) - MAV.C_L_delta_e*cos(alpha)
+    Xu = ustar*MAV.rho*MAV.S_wing/(MAV.mass)*(MAV.C_)
+    return A_lon, B_lon, A_lat, B_lat
 
 def euler_state(x_quat):
     # convert state x with attitude represented by quaternion
